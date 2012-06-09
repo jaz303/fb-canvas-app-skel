dbd51d034fe101a1adfdee76c517c218caff45d4
Jason Frame <jason@onehackoranother.com>
2012-06-10 00:35:06 +0100

    set error display mode based on env

--- fb_skel/boot.php
+++ fb_skel/boot.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-
 date_default_timezone_set("Europe/London");
 
 //
--- fb_skel/env/production.php
+++ fb_skel/env/production.php
@@ -2,4 +2,6 @@
 define('PDO_DSN', 'mysql:host=localhost;dbname=test');
 define('PDO_USERNAME', 'root');
 define('PDO_PASSWORD', '');
+
+ini_set('display_errors', false);
 ?>
