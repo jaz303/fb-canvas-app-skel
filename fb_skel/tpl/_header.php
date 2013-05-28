ef5f9ae9567b6dca3444a5d5be3256384e70dfcf
Jason Frame <jason@onehackoranother.com>
2013-05-28 12:46:05 +0100

    Use app title in header. Use frictionless requests

--- fb_skel/tpl/_header.php
+++ fb_skel/tpl/_header.php
@@ -2,7 +2,8 @@
 
 <html>
   <head>
-    <title>Magners Fridays</title>
+    <title><?= APP_TITLE ?></title>
+    <link rel="stylesheet" href="<?= url_for_stylesheet('reset.css') ?>">
     <link rel="stylesheet" href="<?= url_for_stylesheet('main.css') ?>">
     <script type="text/javascript" src="<?= url_for_javascript('jquery.min.js') ?>"></script>
     <script type="text/javascript" src="<?= url_for_javascript('app.js') ?>"></script>
@@ -12,11 +13,12 @@
     <script>
       window.fbAsyncInit = function() {
         FB.init({
-          appId      : '<?= FB_APP_ID ?>',
-          channelUrl : '<?= json_encode(absolute_url(url_for_app('channel.php'))) ?>',
-          status     : true, // check login status
-          cookie     : false, // enable cookies to allow the server to access the session
-          xfbml      : true  // parse XFBML
+          appId                 : '<?= FB_APP_ID ?>',
+          channelUrl            : '<?= json_encode(absolute_url(url_for_app('channel.php'))) ?>',
+          frictionlessRequests  : true,
+          status                : true, // check login status
+          cookie                : false, // enable cookies to allow the server to access the session
+          xfbml                 : true  // parse XFBML
         });
         
         if (typeof window.onFacebookReady == 'function') {
