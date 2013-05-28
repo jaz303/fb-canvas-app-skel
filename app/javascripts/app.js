4ce13972e76800198da8143f8e143b295f06dfe4
Jason Frame <jason@onehackoranother.com>
2013-05-28 11:35:44 +0100

    Remove text from old app

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -3,11 +3,11 @@ function share() {
      if (response.status === 'connected') {
        var res = FB.ui({
            method: 'feed',
-           name: "Fancy a pint? Have one on us.",
-           link: 'http://apps.facebook.com/magners_fridays/',
-           picture: 'https://magnersfridays.com/app/images/share-icon.png',
-           caption: "Each Friday we'll be offering you the chance to claim a free pint of Magners Original Cider to enjoy in participating bars around Northern Ireland!",
-           description: "(Terms and Conditions apply)"
+           name: "Share Name",
+           link: 'http://localhost/#',
+           picture: '',
+           caption: "Share text...",
+           description: "Share description..."
        });
      }
    });
